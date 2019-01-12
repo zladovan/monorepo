5ba22c127a81a64a6681ac65db0919c06e4387d1
Jan Cabala <jan.cabala@wirecard.com>
2019-01-12 23:30:52 +0100

    Modify server's greeting

--- apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
+++ apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
@@ -5,7 +5,7 @@ import com.zlad.monorepo.common.Commons;
 public class ServerApp {
 
 	public static void main(String[] args) {
-		System.out.println("Good day, I will be the server when I will grow up.");
+		System.out.println("Hi, I will be the server when I will grow up.");
 		System.out.println("For now I know our commons: " + Commons.tellMeWhoWeAre());
 	}
 }
