5b7502b0647b2bc378f0a5c95de78075dce22bc2
zladovan <cabala.jan@gmail.com>
2019-04-06 19:58:24 +0200

    add another library to make it more interesting

--- apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
+++ apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
@@ -1,11 +1,12 @@
 package com.zlad.monorepo.server;
 
 import com.zlad.monorepo.common.Commons;
+import com.zlad.monorepo.logging.Logger;
 
 public class ServerApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hi, I will be the server when I will grow up...");
-		System.out.println("For now I know our commons: " + Commons.tellMeWhoWeAre());
+		Logger.info("Hi, I will be the server when I will grow up...");
+		Logger.info("For now I know our commons: " + Commons.tellMeWhoWeAre());
 	}
 }
---
+++ libs/logging/src/main/java/com/zlad/monorepo/logging/Logger.java
@@ -0,0 +1,8 @@
+package com.zlad.monorepo.logging;
+
+public class Logger {
+
+    public static void info(String message) {
+        System.out.println(message);
+    }
+}
