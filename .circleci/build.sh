d8b7252bc10062d586a5b38ecb331f818da5248e
zladovan <cabala.jan@gmail.com>
2019-04-08 14:40:30 +0200

    add support for resolving symlinks for finding base script directory

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,8 +10,14 @@
 
 set -e
 
-# Find script directory (no support for symlinks)
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Find script directory
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
 # Configuration with default values
 : "${CI:=$DIR/circleci.sh}"
