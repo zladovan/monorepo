b647dc5e0d584651facd1095b30ed99d922fb72f
zladovan <cabala.jan@gmail.com>
2019-04-08 15:15:11 +0200

    try to fix ci tool path variable

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -10,18 +10,12 @@
 
 set -e
 
-# Find script directory
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+# Find script directory (no support for symlinks)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Configuration with default values
-# : "${CI:=$DIR/circleci.sh}"
-CI="${CI:-$DIR/circleci.sh}"
+: "${CI:="$DIR/circleci.sh"}"
+# CI="${CI:-$DIR/circleci.sh}"
 # CI="$DIR/circleci.sh"
 
 echo "${CI} hash last"
