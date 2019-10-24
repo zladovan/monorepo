adbedd7163791a9cbc1c79fe7a45e7df20729cda
zladovan <cabala.jan@gmail.com>
2019-10-24 11:13:45 +0200

    trigger server build

--- apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
+++ apps/server/src/main/java/com/zlad/monorepo/server/ServerApp.java
@@ -6,7 +6,7 @@ import com.zlad.monorepo.logging.Logger;
 public class ServerApp {
 
 	public static void main(String[] args) {
-		Logger.info("Hi, I will be the server when I will grow up...");
+		Logger.info("Hi, I will be the server when I will grow up.");
 		Logger.info("For now I know our commons: " + Commons.tellMeWhoWeAre());
 	}
 }
